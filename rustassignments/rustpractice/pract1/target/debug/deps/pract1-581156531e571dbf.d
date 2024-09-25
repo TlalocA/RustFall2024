db2aff145b4acf3169a0f7989c6e476847a0994c
Tlaloc <tlaloc.alarcon01@utrgv.edu>
2024-09-25 15:20:21 +0000

    moved folders

---
+++ rustassignments/rustpractice/pract1/target/debug/deps/pract1-581156531e571dbf.d
@@ -0,0 +1,5 @@
+/workspaces/RustFall2024/rustpractice/pract1/target/debug/deps/pract1-581156531e571dbf: src/main.rs
+
+/workspaces/RustFall2024/rustpractice/pract1/target/debug/deps/pract1-581156531e571dbf.d: src/main.rs
+
+src/main.rs:
