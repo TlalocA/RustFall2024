00bc2090c2c478110c671e3e16b8d3bd2c59e218
Tlaloc <tlaloc.alarcon01@utrgv.edu>
2024-09-30 20:16:55 +0000

    Testing file I/O

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,26 +1,16 @@
-use std::fs::File;
-use std::io::{Read, BufReader, BufRead};
+use std::fs::OpenOptions;
+use std::io::Write;
 
-fn read_entire_file() {
-    let mut file = File::open("example.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    println!("File contents:\n{}", contents);
-}
+fn append_to_file() {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .open("example.txt")
+        .unwrap();
 
-fn read_file_line_by_line() {
-    let file = File::open("example.txt").unwrap();
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        println!("{}", line.unwrap());
-    }
+    writeln!(file, "This line is appended to the file.").unwrap();
 }
 
 fn main() {
-    println!("Reading entire file:");
-    read_entire_file();
-
-    println!("\nReading file line by line:");
-    read_file_line_by_line();
+    append_to_file();
+    println!("Successfully appended to the file.");
 }
--- rustpractice/pract1/target/debug/pract1.d
+++
@@ -1,1 +0,0 @@
-/workspaces/RustFall2024/rustpractice/pract1/target/debug/pract1: /workspaces/RustFall2024/rustpractice/pract1/src/main.rs
