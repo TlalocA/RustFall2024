e43c9644b3bdc8d4b46ce6667685286cd6f0d6fb
Tlaloc <tlaloc.alarcon01@utrgv.edu>
2024-10-02 15:29:18 +0000

    Lecture 10/2/24

---
+++ hello_world/my_script.py
@@ -0,0 +1,3 @@
+print("Hello World")
+for num in range(1,10):
+	print(num)
--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,16 +1,18 @@
-use std::fs::OpenOptions;
-use std::io::Write;
+use std::process::Command;
 
-fn append_to_file() {
-    let mut file = OpenOptions::new()
-        .append(true)
-        .open("example.txt")
-        .unwrap();
+// not exactly working?
 
-    writeln!(file, "This line is appended to the file.").unwrap();
+fn executing_os_commands_linux() {
+    let output = Command::new(program: "python3")
+        .arg("my_script.py")
+        .output()
+        .expect("Failed to execute command");
+
+    println!("{:?}", output);
+    println!("Command output: {}", String::from_utf8_lossy(&output.stdout));
 }
 
 fn main() {
-    append_to_file();
-    println!("Successfully appended to the file.");
+    executing_os_commands_linux();
+    
 }
