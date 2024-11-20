3fed37e22769a70c4f050e253f618db104f456aa
Tlaloc <tlaloc.alarcon01@utrgv.edu>
2024-11-20 16:38:23 +0000

    implemented loop and fixed retrival method (refactor later)

--- rustassignments/ProjectDataFetch/project/src/main.rs
+++ rustassignments/ProjectDataFetch/project/src/main.rs
@@ -1,20 +1,13 @@
 use serde::Deserialize;
-use std::fs::File;
-use std::io::{Write, BufReader, BufRead};
-use std::time::{Duration, Instant};
+//use std::fs::File;
+//use std::io::{Write, BufReader, BufRead};
+use std::{thread, time::Duration};
 // use std::error::Error;
 
 #[derive(Debug)]
 struct Bitcoin{
     api_address: String,
     file_name:String,
-}
-
-#[derive(Debug)]
-enum ApiResult {
-    Success(BTCPriceAPI),
-    ApiError(String),
-    NetworkError(String),
 }
 
 #[derive(Debug)]
@@ -61,11 +54,11 @@ impl Pricing for Bitcoin {
     }
     
     fn save_to_file(&self){
-        /*
-        let price = self.fetch_price();
-        let content = format!("{{\"bitcoin\": {}}}\n", price);
-        std::fs::write(&self.file_name, content).expect("Unable to write to file");
-        */
+        
+        //let price = self.fetch_price();
+        //let content = format!("{{\"bitcoin\": {}}}\n", price);
+        //std::fs::write(&self.file_name, content).expect("Unable to write to file");
+        
         println!("Saved Bitcoin price to {}", self.file_name);
     }
 }
@@ -103,7 +96,28 @@ impl Pricing for Ethereum {
 
 impl Pricing for SP500 {
     fn fetch_price(&self) -> f32{
-        return 32.0;
+        match ureq::get(&self.api_address).call() {
+            Ok(response) => {
+                if response.status() == 200 {
+                    match response.into_json::<SP500PriceAPI>() {
+                        Ok(data) => {
+                            return data.sp500.usd as f32;
+                        },
+                        Err(e) => {
+                            eprintln!("Failed to parse JSON: {}", e);
+                            0.0
+                        },
+                    }
+                } else {
+                    eprintln!("HTTP error: {}", response.status());
+                    0.0
+                }
+            },
+            Err(e) => {
+                eprintln!("Request failed: {}", e);
+                0.0
+            },
+        }
     }
     
     fn save_to_file(&self){
@@ -129,33 +143,38 @@ struct SP500PriceAPI{
 }
 
 fn main() {
+    // initializing bitcoin api and file
     let btc_api = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd".to_string();
     let btc_txt = "btc_prices.json".to_string();
     let b = Bitcoin{api_address:btc_api, file_name:btc_txt};
     
-    let btc_price = ureq::get(&b.api_address).call().unwrap();
+    //let btc_price = ureq::get(&b.api_address).call().unwrap();
+    //let b: BTCPriceAPI = btc_price.into_json::<BTCPriceAPI>().unwrap();
 
-    let b: BTCPriceAPI = btc_price.into_json::<BTCPriceAPI>().unwrap();
-
-    println!("{:?}", b);
-
+    // initializing ethereum api and file
     let eth_api = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd".to_string();
     let eth_txt = "eth_prices.json".to_string();
     let e = Ethereum{api_address:eth_api, file_name:eth_txt};
 
-    let eth_price = ureq::get(&e.api_address).call().unwrap();
+    //let eth_price = ureq::get(&e.api_address).call().unwrap();
+    //let e: ETHPriceAPI = eth_price.into_json::<ETHPriceAPI>().unwrap();
 
-    let e: ETHPriceAPI = eth_price.into_json::<ETHPriceAPI>().unwrap();
-
-    println!("{:?}", e);
-
-    /*
+    // initializing sp500 api and file
     let sp_api = "https://query1.finance.yahoo.com/v8/finance/chart/%5EGSPC?interval=1m&range=1d".to_string();
     let sp_txt = "sp_prices.json".to_string();
     let s = SP500{api_address:sp_api, file_name:sp_txt};
 
-    let sp_price = ureq::get(&s.api_address).call().unwrap();
+    //let sp_price = ureq::get(&s.api_address).call().unwrap();
 
-    println!("{:?}", s);
-    */
+    // loop to update pricing every 10 seconds
+    // same as while(true)
+    loop {
+        println!("Fetching and processing prices...");
+
+        println!("BTC: {:?} USD", b.fetch_price());
+        println!("ETH: {:?} USD", e.fetch_price());
+        println!("SP500: {:?} USD", s.fetch_price());
+
+        thread::sleep(Duration::from_secs(10));
+    }
 }
